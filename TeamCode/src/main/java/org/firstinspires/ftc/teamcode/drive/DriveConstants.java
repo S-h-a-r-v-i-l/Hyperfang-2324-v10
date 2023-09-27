d28763b0604289593b301c6d0588af403e63dd34
sophiethompson212 <sophie.thompson212@gmail.com>
2023-09-27 07:24:47 -0500

    Update DriveConstants.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -67,7 +67,7 @@ public class DriveConstants {
      */
     public static double MAX_VEL = 48.31435707;
     public static double MAX_ACCEL = 48.31435707;
-    public static double MAX_ANG_VEL = Math.toRadians(60);
+    public static double MAX_ANG_VEL = Math.toRadians(3.22);
     public static double MAX_ANG_ACCEL = Math.toRadians(60);
 
     /*
