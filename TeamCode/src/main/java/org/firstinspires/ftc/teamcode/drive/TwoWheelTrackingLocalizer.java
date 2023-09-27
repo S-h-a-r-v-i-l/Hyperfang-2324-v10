8484c1ffef6d7ac16941b82fab321783f00aa998
S-h-a-r-v-i-l <sarwatesharvil@gmail.com>
2023-09-27 17:15:06 -0500

    ADDING MULTIPLIERS!!!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/TwoWheelTrackingLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/TwoWheelTrackingLocalizer.java
@@ -45,8 +45,8 @@ public class TwoWheelTrackingLocalizer extends TwoTrackingWheelLocalizer {
     public static double PERPENDICULAR_X = 4;
     public static double PERPENDICULAR_Y = 0;
 
-    public static double X_MULTIPLIER = 1; // Multiplier in the X direction
-    public static double Y_MULTIPLIER = 1; // Multiplier in the Y direction
+    public static double X_MULTIPLIER = 0.55957803861; // Multiplier in the X direction
+    public static double Y_MULTIPLIER = -0.830298842; // Multiplier in the Y direction
 
 
     // Parallel/Perpendicular to the forward axis
