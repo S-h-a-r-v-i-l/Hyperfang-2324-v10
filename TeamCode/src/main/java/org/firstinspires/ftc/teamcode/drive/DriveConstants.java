63caba5943cd11817bdd27c86c2d5df55a91e219
S-h-a-r-v-i-l <sarwatesharvil@gmail.com>
2023-10-03 07:05:56 -0500

    Track width + Max ang vel changes
    so much pain :(

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -46,7 +46,7 @@ public class DriveConstants {
      */
     public static double WHEEL_RADIUS = 1.476378; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 15; // in
+    public static double TRACK_WIDTH = 11.2; // in
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
@@ -67,7 +67,7 @@ public class DriveConstants {
      */
     public static double MAX_VEL = 48.31435707;
     public static double MAX_ACCEL = 24;
-    public static double MAX_ANG_VEL = Math.toRadians(180);
+    public static double MAX_ANG_VEL = 2.842705249786377;
     public static double MAX_ANG_ACCEL = Math.toRadians(180);
 
     /*
