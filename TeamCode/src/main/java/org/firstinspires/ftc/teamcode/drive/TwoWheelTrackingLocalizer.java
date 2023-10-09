e984a7b910d50e25482160b66c678b251698ede6
sophiethompson212 <sophie.thompson212@gmail.com>
2023-10-09 14:57:20 -0500

    Update TwoWheelTrackingLocalizer.java
    Switching up the track width and dead wheel positions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/TwoWheelTrackingLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/TwoWheelTrackingLocalizer.java
@@ -38,12 +38,12 @@ public class TwoWheelTrackingLocalizer extends TwoTrackingWheelLocalizer {
     public static double WHEEL_RADIUS = 1.476378; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
 
-    public static double TRACK_WIDTH = 15;
-    public static double PARALLEL_X = 5.5; // X is the up and down direction
-    public static double PARALLEL_Y = 5; // Y is the strafe direction
+    public static double TRACK_WIDTH = 15.25;
+    public static double PARALLEL_X = 5.25; // X is the up and down direction
+    public static double PARALLEL_Y = 5.5; // Y is the strafe direction
 
-    public static double PERPENDICULAR_X = 4;
-    public static double PERPENDICULAR_Y = 0;
+    public static double PERPENDICULAR_X = 4.6;
+    public static double PERPENDICULAR_Y = 0.1;
 
     public static double X_MULTIPLIER = 0.8141; // Multiplier in the X direction
     public static double Y_MULTIPLIER = 0.8308; // Multiplier in the Y direction
