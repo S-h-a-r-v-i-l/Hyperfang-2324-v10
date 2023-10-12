925c892a21024eeb443551a78a7d4e47bd3a1e76
sophiethompson212 <sophie.thompson212@gmail.com>
2023-10-12 07:30:49 -0500

    Update TwoWheelTrackingLocalizer.java
    Changing some of the X and Y values, parallel wheels might change back, but the perpendicular should be negative for the X value

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/TwoWheelTrackingLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/TwoWheelTrackingLocalizer.java
@@ -39,10 +39,10 @@ public class TwoWheelTrackingLocalizer extends TwoTrackingWheelLocalizer {
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
 
     public static double TRACK_WIDTH = 10.5;
-    public static double PARALLEL_X = 5.25; // X is the up and down direction
-    public static double PARALLEL_Y = 5.5; // Y is the strafe direction
+    public static double PARALLEL_X = 5; // X is the up and down direction
+    public static double PARALLEL_Y = 5.25; // Y is the strafe direction
 
-    public static double PERPENDICULAR_X = 4.6;
+    public static double PERPENDICULAR_X = -4.6;
     public static double PERPENDICULAR_Y = 0.1;
 
     public static double X_MULTIPLIER = 0.8141; // Multiplier in the X direction
