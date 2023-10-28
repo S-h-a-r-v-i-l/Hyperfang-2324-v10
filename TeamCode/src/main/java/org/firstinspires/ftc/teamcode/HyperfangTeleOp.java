7296e7ce121c5f7d27d3af7267e643e7e696e294
S-h-a-r-v-i-l <sarwatesharvil@gmail.com>
2023-10-28 13:52:04 -0500

    TeleOp

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HyperfangTeleOp.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+public class HyperfangTeleOp extends LinearOpMode{
+    public DcMotorEx fl = null;
+    public DcMotorEx bl = null;
+    public DcMotorEx fr = null;
+    public DcMotorEx br = null;
+    public DcMotorEx gl = null;
+    public DcMotorEx gr = null;
+
+    public CRServo psl = null;
+    public CRServo ps2 = null;
+
+    public void runOpMode() {
+
+    }
+}
