4ce508136ae1cc32cfbbb773c438a9889eabbfe8
S-h-a-r-v-i-l <sarwatesharvil@gmail.com>
2023-11-06 16:22:13 -0600

    More updates to teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverOpmodes/HyperfangTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverOpmodes/HyperfangTeleOp.java
@@ -3,14 +3,16 @@ package org.firstinspires.ftc.teamcode.DriverOpmodes;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 public class HyperfangTeleOp extends LinearOpMode{
     public DcMotorEx fl = null;
     public DcMotorEx bl = null;
     public DcMotorEx fr = null;
     public DcMotorEx br = null;
-    public DcMotorEx gl = null;
-    public DcMotorEx gr = null;
+    public DcMotorEx sl = null;
+    public DcMotorEx sr = null;
+    public DcMotorEx intake = null;
 
     public CRServo psl = null;
     public CRServo psr = null;
@@ -27,15 +29,16 @@ public class HyperfangTeleOp extends LinearOpMode{
         bl = hardwareMap.get(DcMotorEx.class, "leftBack");
         fr = hardwareMap.get(DcMotorEx.class, "rightFront");
         br = hardwareMap.get(DcMotorEx.class, "rightBack");
-        gl = hardwareMap.get(DcMotorEx.class, "liftLeft");
-        gr = hardwareMap.get(DcMotorEx.class, "liftRight");
+        sl = hardwareMap.get(DcMotorEx.class, "liftLeft");
+        sr = hardwareMap.get(DcMotorEx.class, "liftRight");
         psl = hardwareMap.get(CRServo.class, "percyServoLeft");
         psr = hardwareMap.get(CRServo.class, "percyServoRight");
         intake = hardwareMap.get(DcMotorEx.class, "intake");
 
         fl.setDirection(DcMotorEx.Direction.REVERSE);
         bl.setDirection(DcMotorEx.Direction.REVERSE);
-        gr.setDirection(DcMotorEx.Direction.REVERSE);
+        sr.setDirection(DcMotorEx.Direction.REVERSE);
+        psr.setDirection(CRServo.Direction.REVERSE);
 
         while (opModeIsActive()) {
             drive = -gamepad1.left_stick_y;
@@ -57,10 +60,12 @@ public class HyperfangTeleOp extends LinearOpMode{
             fr.setPower(right);
             br.setPower(right);
 
-            gl.setPower(gamepad2.right_stick_y);
-            gr.setPower(gamepad2.right_stick_y);
+            sl.setPower(gamepad2.right_stick_y);
+            sr.setPower(gamepad2.right_stick_y);
             intake.setPower(gamepad2.right_trigger);
 
+            psl.setPower(gamepad2.left_stick_y);
+            psr.setPower(gamepad2.left_stick_y);
 
             sleep(50);
 
