280d1a16884d435e1e55103afb7a626bc3e09ca8
sophiethompson212 <sophie.thompson212@gmail.com>
2023-11-08 07:56:24 -0600

    auto (????)
    I'm fully expecting to delete most of this later today but I was trying to look at other codes and copy down something sooooo yeah
    
    FEEL FREE TO DELETE LATER

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCV/OpenCVprotoRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCV/OpenCVprotoRed.java
@@ -1,9 +1,14 @@
 package org.firstinspires.ftc.teamcode.OpenCV;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.Rect;
@@ -14,6 +19,8 @@ import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
 import org.openftc.easyopencv.OpenCvPipeline;
 import org.openftc.easyopencv.OpenCvWebcam;
+
+import java.util.Base64;
 
 @Autonomous
 public class OpenCVprotoRed extends OpMode {
@@ -97,7 +104,73 @@ public class OpenCVprotoRed extends OpMode {
             return outPut;
 
         }
+
+        private DcMotor frontRight;
+        private DcMotor backRight;
+        private DcMotor frontLeft;
+        private DcMotor backLeft;
+
+        private ElapsedTime runtime = new ElapsedTime();
+
+        /*
+        private Base64.Decoder vuforia;
+
+        OpticalDistanceSensor odsFL;
+
+        BNO055IMU imu;
+
+        Orientation angles;
+        @Override
+        public void runOpMode() throws InterruptedException {
+            vuforia = new Base64.Decoder(hardwareMap, "AYx3Kw3/////AAAAGQreNEJhLkdWqUbBsQ06dnWIksoccLxh/R9WNkXB8hvuonWmFXUWJ2tYqM+8VqYCWXkHfanXzG/G1un7ZvwgGkkO6u0ktevZDb8AFWF2/Y4wVH1BWGQ2psV5QkHAKZ7Z6ThZI01HPZqixiQowyeUstv7W/QU8jJ48NrqGBLVYdE6eFfzNDzVY/1IvrBJaRwqKR8vo+3a2zmeFEnEhFTqMI7anU2WSPy8RP7tR61CdfidjL2biMe0RiSOBIbqOe4rs9NGaDvp1Crtz17uyY71GyMkp+Kmjbejyfj8LgZ/dZQoEsuVuQyo0dbd4KBxsEJlQj/uAEst22QoEwZe0Af4DnFtwn6/IEe02L3DT3/Np+ZX");
+
+            BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+            parameters.angleUnit            = BNO055IMU.AngleUnit.DEGREES;
+
+            OpticalDistanceSensor odsFL = hardwareMap.get(OpticalDistanceSensor.class, "odsFL");
+
+            imu = hardwareMap.get(BNO055IMU.class, "imu");
+            imu.initialize(parameters);
+
+            waitForStart();
+            runtime.reset();
+        }
+
+        public void auto() throws InterruptedException{
+            vuforia.start();
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("Status", "Target: " + vuforia.getDecodedColumn());
+            telemetry.addData("Status", "Name: " + vuforia.getMark().name());
+            telemetry.update();
+
+            int column = vuforia.getDecodedColumn();
+
+            if (leftavgfin > rightavgfin && leftavgfin > midavgfin) {
+                switch (column) {
+                    case 1:
+                }
+            } else if (midavgfin > leftavgfin && midavgfin > rightavgfin) {
+                switch (column) {
+                    case 2:
+                }
+            } else {
+                switch (column) {
+                    case 3:
+                }
+            }
+        }
+        public void modeResetEncoder() throws InterruptedException {
+                frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        }
+
+        */
     }
+
+
+
 }
 
 
