efa8cf0e7ecece991074573e092717ed551c0956
MarielleCairo <planetmarielle@gmail.com>
2023-11-13 16:23:47 -0600

    changed boxes for vision

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCV/OpenCVprotoBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCV/OpenCVprotoBlue.java
@@ -1,196 +1,196 @@
-//package org.firstinspires.ftc.teamcode.OpenCV;
-//
-//import static android.os.SystemClock.sleep;
-//
-//import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-//import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-//import com.qualcomm.robotcore.hardware.CRServo;
-//import com.qualcomm.robotcore.hardware.DcMotor;
-//import com.qualcomm.robotcore.hardware.DcMotorEx;
-//
-//import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-//import org.opencv.core.Core;
-//import org.opencv.core.Mat;
-//import org.opencv.core.Rect;
-//import org.opencv.core.Scalar;
-//import org.opencv.imgproc.Imgproc;
-//import org.openftc.easyopencv.OpenCvCamera;
-//import org.openftc.easyopencv.OpenCvCameraFactory;
-//import org.openftc.easyopencv.OpenCvCameraRotation;
-//import org.openftc.easyopencv.OpenCvPipeline;
-//import org.openftc.easyopencv.OpenCvWebcam;
-//
-//@Autonomous
-//public class OpenCVprotoBlue extends OpMode {
-//    public DcMotorEx fl = null;
-//    public DcMotorEx bl = null;
-//    public DcMotorEx fr = null;
-//    public DcMotorEx br = null;
-//    public DcMotorEx ll = null;
-//    public DcMotorEx lr = null;
-//    public DcMotorEx intake = null;
-//
-//    public CRServo psl = null;
-//    public CRServo psr = null;
-//    OpenCvWebcam webcam1 = null;
-//    int zone;
-//
-//    @Override
-//    public void init() {
-//        fl = hardwareMap.get(DcMotorEx.class, "frontLeft");
-//        bl = hardwareMap.get(DcMotorEx.class, "backLeft");
-//        fr = hardwareMap.get(DcMotorEx.class, "frontRight");
-//        br = hardwareMap.get(DcMotorEx.class, "backRight");
-//        ll = hardwareMap.get(DcMotorEx.class, "leftLift");
-//        lr = hardwareMap.get(DcMotorEx.class, "rightLift");
-//        psl = hardwareMap.get(CRServo.class, "percyL");
-//        psr = hardwareMap.get(CRServo.class, "percyR");
-//        intake = hardwareMap.get(DcMotorEx.class, "intake");
-//
-//        fl.setDirection(DcMotorEx.Direction.REVERSE);
-//        bl.setDirection(DcMotorEx.Direction.REVERSE);
-//        psl.setDirection(CRServo.Direction.REVERSE);
-//
-//        fl.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
-//        bl.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
-//        fr.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
-//        br.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
-//        fl.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
-//        bl.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
-//        fr.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
-//        br.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
-//
-//
-//
-//        WebcamName webcamName = hardwareMap.get(WebcamName.class, "webcam1");
-//        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-//        webcam1 = OpenCvCameraFactory.getInstance().createWebcam(webcamName, cameraMonitorViewId);
-//
-//        webcam1.setPipeline(new PipelineBlue());
-//
-//        webcam1.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
-//            @Override
-//            public void onOpened() {
-//                webcam1.startStreaming(1280, 720, OpenCvCameraRotation.UPRIGHT);
-//            }
-//
-//            @Override
-//            public void onError(int errorCode) {
-//
-//            }
-//        });
-//    }
-//
-//    @Override
-//    public void loop() {
-//
-//    }
-//
-//    class PipelineBlue extends OpenCvPipeline{
-//
-//        Mat YCbCr = new Mat();
-//        Mat midCrop;
-//        Mat leftCrop;
-//        Mat rightCrop;
-//        double leftavgfin;
-//        double rightavgfin;
-//        double midavgfin;
-//        Mat outPut = new Mat();
-//        Scalar rectColor = new Scalar(255.0, 0.0, 0.0);
-//
-//        @Override
-//        public Mat processFrame(Mat input) {
-//
-//            Imgproc.cvtColor(input, YCbCr, Imgproc.COLOR_RGB2YCrCb);
-//            telemetry.addLine("Pipeline running");
-//
-//            Rect leftRect = new Rect(1, 1, 213, 359);
-//            Rect rightRect = new Rect(213, 1, 213, 359);
-//            Rect midRect = new Rect(426, 1, 213, 359);
-//
-//
-//            input.copyTo(outPut);
-//            Imgproc.rectangle(outPut, leftRect, rectColor, 2);
-//            Imgproc.rectangle(outPut, rightRect, rectColor, 2);
-//            Imgproc.rectangle(outPut, midRect, rectColor, 2);
-//
-//            leftCrop = YCbCr.submat(leftRect);
-//            rightCrop = YCbCr.submat(rightRect);
-//            midCrop = YCbCr.submat(midRect);
-//
-//            Scalar leftavg = Core.mean(leftCrop);
-//            Scalar rightavg = Core.mean(rightCrop);
-//            Scalar midavg = Core.mean(midCrop);
-//
-//            leftavgfin = leftavg.val[2];
-//            rightavgfin = rightavg.val[2];
-//            midavgfin = midavg.val[2];
-//
-//            if (leftavgfin > rightavgfin && leftavgfin > midavgfin) {
-//                telemetry.addLine("Left");
-//                zone = 1;
-//            } else if (midavgfin > leftavgfin && midavgfin > rightavgfin) {
-//                telemetry.addLine("middle");
-//                zone = 2;
-//            } else {
-//                telemetry.addLine("Right");
-//                zone = 3;
-//            }
-//
-//            return outPut;
-//
-//        }
-//    }
-//
-//    @Override
-//    public void start() {
-//        if (zone == 1) {
-//
-//        } else if (zone == 2) {
-//
-//        } else {
-//            fl.setTargetPosition(1738);
-//            bl.setTargetPosition(1738);
-//            fr.setTargetPosition(1738);
-//            br.setTargetPosition(1738);
-//
-//            sleep(1000);
-//
-//            psl.setPower(1);
-//            psr.setPower(1);
-//            sleep(500);
-//
-//        }
-//    }
-//}
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
+package org.firstinspires.ftc.teamcode.OpenCV;
+
+import static android.os.SystemClock.sleep;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvPipeline;
+import org.openftc.easyopencv.OpenCvWebcam;
+
+@Autonomous
+public class OpenCVprotoBlue extends OpMode {
+    public DcMotorEx fl = null;
+    public DcMotorEx bl = null;
+    public DcMotorEx fr = null;
+    public DcMotorEx br = null;
+    public DcMotorEx ll = null;
+    public DcMotorEx lr = null;
+    public DcMotorEx intake = null;
+
+    public CRServo psl = null;
+    public CRServo psr = null;
+    OpenCvWebcam webcam1 = null;
+    int zone;
+
+    @Override
+    public void init() {
+        fl = hardwareMap.get(DcMotorEx.class, "frontLeft");
+        bl = hardwareMap.get(DcMotorEx.class, "backLeft");
+        fr = hardwareMap.get(DcMotorEx.class, "frontRight");
+        br = hardwareMap.get(DcMotorEx.class, "backRight");
+        ll = hardwareMap.get(DcMotorEx.class, "leftLift");
+        lr = hardwareMap.get(DcMotorEx.class, "rightLift");
+        psl = hardwareMap.get(CRServo.class, "percyL");
+        psr = hardwareMap.get(CRServo.class, "percyR");
+        intake = hardwareMap.get(DcMotorEx.class, "intake");
+
+        fl.setDirection(DcMotorEx.Direction.REVERSE);
+        bl.setDirection(DcMotorEx.Direction.REVERSE);
+        psl.setDirection(CRServo.Direction.REVERSE);
+
+        fl.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        bl.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        fr.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        br.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        fl.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        bl.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        fr.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+        br.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+
+
+
+        WebcamName webcamName = hardwareMap.get(WebcamName.class, "webcam1");
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcam1 = OpenCvCameraFactory.getInstance().createWebcam(webcamName, cameraMonitorViewId);
+
+        webcam1.setPipeline(new PipelineBlue());
+
+        webcam1.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                webcam1.startStreaming(1280, 720, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+
+            }
+        });
+    }
+
+    @Override
+    public void loop() {
+
+    }
+
+    class PipelineBlue extends OpenCvPipeline{
+
+        Mat YCbCr = new Mat();
+        Mat midCrop;
+        Mat leftCrop;
+        Mat rightCrop;
+        double leftavgfin;
+        double rightavgfin;
+        double midavgfin;
+        Mat outPut = new Mat();
+        Scalar rectColor = new Scalar(255.0, 0.0, 0.0);
+
+        @Override
+        public Mat processFrame(Mat input) {
+
+            Imgproc.cvtColor(input, YCbCr, Imgproc.COLOR_RGB2YCrCb);
+            telemetry.addLine("Pipeline running");
+
+            Rect leftRect = new Rect(1, 1, 426, 720);
+            Rect rightRect = new Rect(426, 1, 846, 720);
+            Rect midRect = new Rect(846, 1, 1270, 720);
+
+
+            input.copyTo(outPut);
+            Imgproc.rectangle(outPut, leftRect, rectColor, 2);
+            Imgproc.rectangle(outPut, rightRect, rectColor, 2);
+            Imgproc.rectangle(outPut, midRect, rectColor, 2);
+
+            leftCrop = YCbCr.submat(leftRect);
+            rightCrop = YCbCr.submat(rightRect);
+            midCrop = YCbCr.submat(midRect);
+
+            Scalar leftavg = Core.mean(leftCrop);
+            Scalar rightavg = Core.mean(rightCrop);
+            Scalar midavg = Core.mean(midCrop);
+
+            leftavgfin = leftavg.val[2];
+            rightavgfin = rightavg.val[2];
+            midavgfin = midavg.val[2];
+
+            if (leftavgfin > rightavgfin && leftavgfin > midavgfin) {
+                telemetry.addLine("Left");
+                zone = 1;
+            } else if (midavgfin > leftavgfin && midavgfin > rightavgfin) {
+                telemetry.addLine("middle");
+                zone = 2;
+            } else {
+                telemetry.addLine("Right");
+                zone = 3;
+            }
+
+            return outPut;
+
+        }
+    }
+
+    @Override
+    public void start() {
+        if (zone == 1) {
+
+        } else if (zone == 2) {
+
+        } else {
+            fl.setTargetPosition(1738);
+            bl.setTargetPosition(1738);
+            fr.setTargetPosition(1738);
+            br.setTargetPosition(1738);
+
+            sleep(1000);
+
+            psl.setPower(1);
+            psr.setPower(1);
+            sleep(500);
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
