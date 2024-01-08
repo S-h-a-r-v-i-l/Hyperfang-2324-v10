a1f457be6edc7c0f398a734d07d441aab598139c
Y-i-m-i-n-g <yiming.peng.yp@gmail.com>
2024-01-08 14:35:44 -0600

    Gear Ratio off?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -47,7 +47,7 @@ public class DriveConstants {
      * convenience. Make sure to exclude any gear ratio included in MOTOR_CONFIG from GEAR_RATIO.
      */
     public static double WHEEL_RADIUS = 1.4763; // in
-    public static double GEAR_RATIO = 0.5; // output (wheel) speed / input (motor) speed
+    public static double GEAR_RATIO = 2; // output (wheel) speed / input (motor) speed
     public static double TRACK_WIDTH = 14.5; // in
 
     /*
